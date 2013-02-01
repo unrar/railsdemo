c2c61c3e8fa721f525bf92f64e026b09b5208092
unrar <catbuntu@catbuntu.me>
2013-02-01 20:45:51 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5c998090d46e7192b735b3bfaeb43c9b45cc5290787193962d47da117f32fe0deb464f23bd4b6c5d3ccfc9a64da608190bc692b6ba0b121278a4c43245a713e0'
